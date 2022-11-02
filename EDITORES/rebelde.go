568de51487ee086211044808447d61d4e0c36ff6
canbecoffee <canbecoffee@gmail.com>
2022-11-02 14:58:38 -0300

    Exit despues de cierre
    Cierre despues de exit al namenode

--- EDITORES/rebelde.go
+++ EDITORES/rebelde.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"time"
+	"os"
 
 	pb "github.com/Kendovvul/Ejemplo/Proto"
 	"google.golang.org/grpc"
@@ -88,7 +89,7 @@ func main() {
 		case "2":
 
 			Comunicacion("CIERRE")
-
+			os.Exit(1)
 		default:
 			fmt.Println("Opcion no valida")
 		}
